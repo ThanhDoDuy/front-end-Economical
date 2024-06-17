ff7f7605c93ec08cb6395c9bc9ad1e5c0e62a612
Thanh.Do <duythanh1602@gmail.com>
2024-06-17 21:15:50 +0700

    setup redux

--- src/routers/routes/publicRoute.js
+++ src/routers/routes/publicRoute.js
@@ -1,9 +1,14 @@
 import { lazy } from "react";
 
+const AdminLogin = lazy(() => import("../../views/auth/AdminLogin"));
 const Login = lazy(() => import("../../views/auth/Login"));
 const Register = lazy(() => import("../../views/auth/Register"));
 
 const publicRoutes = [
+    {
+        path: '/admin/login',
+        element: <AdminLogin />
+    },
     {
         path: '/login',
         element: <Login />
--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { FaGoogle, FaFacebook } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { admin_login } from '../../store/Reducers/authReducer';
 
-const Login = () => {
+const AdminLogin = () => {
+    const dispatch = useDispatch();
+
     const [state, setState] = useState({
         email: "",
         password: "",
@@ -17,17 +19,21 @@ const Login = () => {
 
     const submit = (e) => {
         e.preventDefault();
+        dispatch(admin_login(state));
         console.log(state);
+
     };
     return (
         <div className="min-w-screen min-h-screen bg-[#817885] flex justify-center items-center">
             <div className="w-[350px] text-[#ffffff] p-2">
                 <div className="bg-[#6f68d1] p-4 rounded-md">
-                    <h2 className="text-xl mb-3 front-bold">Welcome to E-commerce</h2>
-                    <p className="text-sm mb-3 front-medium">
-                        Please Sign In your account
-                    </p>
-
+                    <div className="mb-4">
+                        <img
+                            src="/images/HappyTail.png"
+                            alt="icon admin shop"
+                            className="w-full h-auto object-contain rounded-md"
+                        />
+                    </div>
                     <form onSubmit={submit}>
                         <div className="flex flex-col w-full gap-1 mb-3">
                             <label htmlFor="email">Email</label>
@@ -60,39 +66,6 @@ const Login = () => {
                         <button
                             className='bg-slate-800 w-full hover:shadow-blue-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'
                         >Sign In</button>
-
-
-                        <div className="flex items-center justify-center gap-3 mb-3">
-                            <p>Don't has an account ?
-                                <Link
-                                    className='front-bold text-inherit bg-transparent border-none cursor-pointer text-lg hover:text-black hover:underline'
-                                    to="/register"
-                                > Sign Up</Link>
-                            </p>
-                        </div>
-
-                        <div className="w-full flex items-center justify-center mb-3">
-                            <div className='w-[45%] bg-slate-700 h-[1px]'></div>
-                            <div className='w-[10%] flex items-center justify-center'>
-                                <span className='pb-1'>Or</span>
-                            </div>
-                            <div className='w-[45%] bg-slate-700 h-[1px]'></div>
-                        </div>
-
-                        <div className="flex items-center justify-center gap-3">
-                            <div
-                                className='w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg
-                     hover:shadow-orange-700/50 justify-center cursor-pointer
-                     items-center overflow-hidden'>
-                                <span className='pb-1'><FaGoogle /></span>
-                            </div>
-                            <div
-                                className='w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg
-                     hover:shadow-orange-700/50 justify-center cursor-pointer
-                     items-center overflow-hidden'>
-                                <span className='pb-1'><FaFacebook /></span>
-                            </div>
-                        </div>
                     </form>
                 </div>
             </div>
@@ -100,4 +73,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default AdminLogin;
