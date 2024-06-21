06185a3aebc5f655d184ac74f769d24ea948a575
Thanh.Do <duythanh1602@gmail.com>
2024-06-21 09:03:01 +0700

    Error and notification Message management

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,25 @@
 import axios from 'axios';
+import store from '../store'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://localhost:5000/v1/api',
 });
+
+// Axios request interceptor.
+// Interceptors are functions that Axios runs before a request is sent or after a response is received
+// this is the request interceptor which executed before a request
+api.interceptors.request.use(
+    (config) => {
+        const state = store.getState();
+        const accessTokentoken = state.auth.accessToken;
+        if (accessTokentoken) {
+            config.headers['Authorization'] = `Bearer ${accessTokentoken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -3,14 +3,13 @@ import api from "../../api/api";
 
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
-    async(info) => {
-        console.log(info);
+    async (info, { fulfillWithValue ,rejectWithValue }) => {
         try {
-            const { data } = await api.post('/admin-login', info,
-                { withCredentials: true});
+            const { data } = await api.post('admin/login', info, { withCredentials: true });
             console.log(data)
+            return fulfillWithValue(data);
         } catch (error) {
-            
+            return rejectWithValue(error.response.data);
         }
     }
 )
@@ -22,13 +21,40 @@ export const authReducer = createSlice({
         errorMessage: '',
         loader: false,
         userInfo: '',
+        status: 'idle',
+        accessToken: null,
+        refreshToken: null,
+        error: null,
     },
     reducers: {
-
+        messageClear: (state, _ ) => {
+            state.errorMessage = '';
+        }
     },
-    extraReducers: () => {
+    extraReducers: (builder) => {
+        builder
+            .addCase(admin_login.pending, (state, { payload }) => {
+                state.loader = true;
+                state.status = 'loading';
+                state.successMessage = '';
 
-    }
+            })
+            .addCase(admin_login.fulfilled, (state, action) => {
+                state.loader = false;
+                state.status = 'succeeded';
+                state.successMessage = action.payload.message;
+                state.user = action.payload.metadata.admin;
+                state.accessToken = action.payload.metadata.tokens.accessToken;
+                state.refreshToken = action.payload.metadata.tokens.refreshToken;
+            })
+            .addCase(admin_login.rejected, (state, action) => {
+                state.loader = false;
+                state.status = 'failed';
+                state.errorMessage = action.payload.message;
+                state.error = action.payload;
+            });
+    },
 });
 
+export const { messageClear } = authReducer.actions;
 export default authReducer.reducer;
